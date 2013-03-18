a0e87f97fa5e3be76cf805c247b25f9c995d4c55
GeorgeM & Diane Wilkins <student@epicodus.com>
2013-03-18 16:06:06 -0700

    adds more functions

--- gitter_ui.rb
+++ gitter_ui.rb
@@ -9,17 +9,17 @@ end
 def menu(login_info)
   choice = nil
   until choice == 'e'
-    puts "What would you like to do:"
+    puts "\nWhat would you like to do:"
     puts "Press 'l' to list your Gists."
     puts "Press 'c' to create a new Gist."
-    puts "Press 'v' to view a Gist."
-    puts "Press 'u' to update a Gist."
+    # puts "Press 'v' to view a Gist."
+    # puts "Press 'u' to update a Gist."
     puts "Press 'd' to delete a Gist."
     puts "Press 'e' to exit."
 
     case choice = gets.chomp
     when 'l'
-      list
+      list(login_info)
     when 'c'
       create(login_info)
     when 'v'
@@ -27,7 +27,7 @@ def menu(login_info)
     when 'u'
       update
     when 'd'
-      delete
+      delete(login_info)
     when 'e'
       exit
     else
@@ -36,19 +36,25 @@ def menu(login_info)
   end
 end
 
-def login
-  print "Username:  "
+def login    # need to validate login info
+  print "\nUsername:  "
   username = gets.chomp
   print "Password:  "
   password =  STDIN.noecho(&:gets).chomp
   puts ''
-  {:username => username, :password => password}
+  {:username => 'epicodusstudent', :password => 'Poiu0987'}
 end
 
-def create(login_info)
+def list(login_info)
+  puts "\nHere is your gist list:"
+  gists = Gist.list(login_info)
+  gists.each {|gist| puts "ID #{gist.id}  Filename: #{gist.filename}   Url: #{gist.url} " }
+end
+
+def create(login_info)   # need to show error msgs
   public_attribute = nil
   while public_attribute.nil?
-    print "Would you like this Gist to be public (y/n): "
+    print "\nWould you like this Gist to be public (y/n): "
     public_attribute = gets.chomp
     case public_attribute
     when 'y'
@@ -67,8 +73,26 @@ def create(login_info)
   puts "Now, type the content of the Gist:"
   content = gets.chomp
   files = {filename => {:content => content}}
-
   Gist.create({:public => public_attribute, :description => description, :files => files}, login_info)
+  # if Gist.create({:public => public_attribute, :description => description, :files => files}, login_info).nil?
+  #   puts "Gist failed..."
+  #   delete(login_info)
+  # else
+  #   puts "Create Successful!"
+  # end
+end
+
+def delete(login_info)    # need to show error msgs
+  list(login_info)
+  print "\nWhich Gist would you like to delete (specify by ID):  "
+  gist_id = gets.chomp
+  Gist.delete(gist_id, login_info)
+  # if Gist.delete(gist_id, login_info).nil?
+  #   puts "Delete failed...did you use a valid ID?"
+  #   delete(login_info)
+  # else
+  #   puts "Delete Successful!"
+  # end
 end
 
 welcome
--- lib/gist.rb
+++ lib/gist.rb
@@ -1,4 +1,12 @@
 class Gist
+  attr_reader :url, :id, :filename, :description
+  def initialize(attributes)
+    @url = attributes['url']
+    @id = attributes['id']
+    @filename = attributes['files'].keys.first
+    @description = attributes['description']
+  end
+
   def self.create(gist, login)
     post_response = Faraday.post do |request|
       request.url 'https://api.github.com/gists'
@@ -13,14 +21,14 @@ class Gist
       request.headers['Authorization'] = "Basic " + Base64.encode64("#{login[:username]}:#{login[:password]}")
       #don't forget to convert this to has via JSON.parse
     end
-    if post_response.body.class == String
-      JSON.parse(post_response.body)
-    else
+    if post_response.body.nil?
       false
+    else
+      JSON.parse(post_response.body).inject([]) { |gist_array, gist_attributes| gist_array << Gist.new(gist_attributes)  }
     end
   end
 
-  def self.delete(login, id)
+  def self.delete(id, login)
     post_response = Faraday.delete do |request|
       request.url "https://api.github.com/gists/#{id}"
       request.headers['Authorization'] = "Basic " + Base64.encode64("#{login[:username]}:#{login[:password]}")
